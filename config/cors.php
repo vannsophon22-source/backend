ceaabe8c6d0d3488f86e917b12d931a9aaf2634f
Vann-sophon <vann.sophon22@gmail.com>
2026-06-12 18:37:22 +0700

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -2,12 +2,20 @@
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'broadcasting/*'],
-'allowed_methods' => ['*'],
-'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000'], // Match your frontend URL
-'allowed_headers' => ['*'],
-    'allowed_origins_patterns' => [],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://renting-room-25rv.vercel.app',
+        'http://localhost:3000',
+        'http://127.0.0.1:3000'
+    ],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
-    'supports_credentials' => true,
+
+    'supports_credentials' => false,
 ];
